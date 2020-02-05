bd0894595579d243625045cf316b281dd8e29732
Hemerson Vianna <hemerson.lourenco@gmail.com>
2020-02-05 18:54:42 -0300

    feat: add dependencies

---
+++ source/server/config/passport.js
@@ -0,0 +1,99 @@
+const passport = require('passport');
+const { Strategy: InstagramStrategy } = require('passport-instagram');
+const moment = require('moment');
+
+passport.use(new InstagramStrategy({
+  clientID: process.env.INSTAGRAM_ID,
+  clientSecret: process.env.INSTAGRAM_SECRET,
+  callbackURL: '/auth/instagram/callback',
+  passReqToCallback: true
+}, (req, accessToken, refreshToken, profile, done) => {
+  if (req.user) {
+    User.findOne({ instagram: profile.id }, (err, existingUser) => {
+      if (err) { return done(err); }
+      if (existingUser) {
+        req.flash('errors', { msg: 'There is already an Instagram account that belongs to you. Sign in with that account or delete it, then link it with your current account.' });
+        done(err);
+      } else {
+        User.findById(req.user.id, (err, user) => {
+          if (err) { return done(err); }
+          user.instagram = profile.id;
+          user.tokens.push({ kind: 'instagram', accessToken });
+          user.profile.name = user.profile.name || profile.displayName;
+          user.profile.picture = user.profile.picture || profile._json.data.profile_picture;
+          user.profile.website = user.profile.website || profile._json.data.website;
+          user.save((err) => {
+            req.flash('info', { msg: 'Instagram account has been linked.' });
+            done(err, user);
+          });
+        });
+      }
+    });
+  } else {
+    User.findOne({ instagram: profile.id }, (err, existingUser) => {
+      if (err) { return done(err); }
+      if (existingUser) {
+        return done(null, existingUser);
+      }
+      const user = new User();
+      user.instagram = profile.id;
+      user.tokens.push({ kind: 'instagram', accessToken });
+      user.profile.name = profile.displayName;
+      // Similar to Twitter API, assigns a temporary e-mail address
+      // to get on with the registration process. It can be changed later
+      // to a valid e-mail address in Profile Management.
+      user.email = `${profile.username}@instagram.com`;
+      user.profile.website = profile._json.data.website;
+      user.profile.picture = profile._json.data.profile_picture;
+      user.save((err) => {
+        done(err, user);
+      });
+    });
+  }
+}));
+
+exports.isAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/login');
+};
+
+exports.isAuthorized = (req, res, next) => {
+  const provider = req.path.split('/')[2];
+  const token = req.user.tokens.find((token) => token.kind === provider);
+  if (token) {
+    if (token.accessTokenExpires && moment(token.accessTokenExpires).isBefore(moment().subtract(1, 'minutes'))) {
+      if (token.refreshToken) {
+        if (token.refreshTokenExpires && moment(token.refreshTokenExpires).isBefore(moment().subtract(1, 'minutes'))) {
+          res.redirect(`/auth/${provider}`);
+        } else {
+          refresh.requestNewAccessToken(`${provider}`, token.refreshToken, (err, accessToken, refreshToken, params) => {
+            User.findById(req.user.id, (err, user) => {
+              user.tokens.some((tokenObject) => {
+                if (tokenObject.kind === provider) {
+                  tokenObject.accessToken = accessToken;
+                  if (params.expires_in) tokenObject.accessTokenExpires = moment().add(params.expires_in, 'seconds').format();
+                  return true;
+                }
+                return false;
+              });
+              req.user = user;
+              user.markModified('tokens');
+              user.save((err) => {
+                if (err) console.log(err);
+                next();
+              });
+            });
+          });
+        }
+      } else {
+        res.redirect(`/auth/${provider}`);
+      }
+    } else {
+      next();
+    }
+  } else {
+    res.redirect(`/auth/${provider}`);
+  }
+};
---
+++ source/server/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const compression = require('compression');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const chalk = require('chalk');
+const errorHandler = require('errorhandler');
+const dotenv = require('dotenv');
+const flash = require('express-flash');
+const passport = require('passport');
+
+dotenv.config({ path: '../../.env.example' });
+
+const app = express();
+const passportConfig = require('./config/passport');
+const apiController = require('./controllers/api');
+
+app.use(compression());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+app.disable('x-powered-by');
+
+app.get('/api/instagram', passportConfig.isAuthenticated, passportConfig.isAuthorized, apiController.getInstagram);
+
+app.get('/auth/instagram', passport.authenticate('instagram', { scope: ['basic', 'public_content'] }));
+app.get('/auth/instagram/callback', passport.authenticate('instagram', { failureRedirect: '/login' }), (req, res) => {
+  res.redirect(req.session.returnTo || '/');
+});
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(errorHandler());
+} else {
+  app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(500).send('Server Error');
+  });
+}
+
+app.listen(app.get('port'), () => {
+  console.log('%s App is running at http://localhost:%d in %s mode', chalk.green('✓'), app.get('port'), app.get('env'));
+  console.log('  Press CTRL-C to stop\n');
+});
+
+module.exports = app;
