c5b4dfbe362108a30d1c526c255bae0c62520fa3
Hemerson Vianna <hemerson.lourenco@gmail.com>
2020-02-07 20:58:51 -0300

    feat: change files from the client folder to the scripts folder

---
+++ source/scripts/index.js
@@ -0,0 +1,10 @@
+const ig = require('./like');
+
+(async () => {
+  await ig.initialize();
+  await ig.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
+  await ig.like([process.env.IG_TAG]);
+})()
+
+// IG_TYPE=profile or tag
+// IG_TYPE=tag IG_USERNAME=username IG_PASSWORD=password IG_TAG=javascript node ./source/scripts
